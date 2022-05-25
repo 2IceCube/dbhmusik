72e4ff91ee4865bf5eb81cd4c9d7a9965bccb6c7
2IceCube <developer@2icecube.de>
2022-05-25 21:43:59 +0200

    Vorlage

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    console.log(`Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size}`);
+    console.log(`Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers.`);
 
     client.user.setActivity(client.config.app.playing);
 };
